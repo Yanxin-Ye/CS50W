92ed0eeabae250c778e5880a76f463c869780ee3
Nicole Ye <nicoleyxye@gmail.com>
2023-08-12 00:32:59 -0400

    YES if today is newyear otherwise NO

--- lecture_django/lecture3/newyear/views.py
+++ lecture_django/lecture3/newyear/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    now = datetime.now()
+    is_newyear = now.month == 1 and now.day == 1
+    # return HttpResponse("new year")
+    return render(request, "newyear/index.html", {"is_newyear": is_newyear})
